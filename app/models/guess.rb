c709c86baee41e347288c293b19fdfbe1bda94fe
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-04-15 13:53:07 -0500

    Adding Guess model and migration with validations

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,7 @@
 class Guess < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :round
+  belongs_to :card
+
+  validates :round, presence: true
+  validates :card, presence: true
 end
--- db/migrate/20160415122104_create_decks.rb
+++ db/migrate/20160415122104_create_decks.rb
@@ -1,4 +1,12 @@
 class CreateDecks < ActiveRecord::Migration
   def change
+    create_table :guesses do |t|
+      t.integer :card_id
+      t.integer :round_id
+      t.string  :response
+      t.boolean :correct
+
+      t.timestamps null: false
+    end
   end
 end
