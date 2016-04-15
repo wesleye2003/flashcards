10eedb1edc5da75fd957031c9a6c18812b66159a
Dave Kaiser & Gregory D. Toprak <pair+davidwkaiser.gdtoprak@devbootcamp.com>
2016-04-15 15:07:58 -0500

    add a functioning game engine

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -24,15 +24,34 @@ end
 
 get '/decks/:deck_id' do
   @deck = Deck.find(params[:deck_id])
-  @card = @deck.cards.sample #
-  # check that card has not been done
+  @possible_cards = @deck.cards.select do |card|
+    guess = Guess.find_by(card: card, round_id: 3)
+    p guess
+    p guess.nil? || (guess.correct == false)
+  end
+
+  redirect "/" if @possible_cards.empty?
+
+  @card = @possible_cards.sample
   erb :'question'
 end
 
 post '/decks/:deck_id' do
-  # if guess correct
-    # mark question in session
-    # session[:cards] <<
+  given_guess = params[:guess]
+  @deck = Deck.find(params[:deck_id])
+  @card = Card.find(params[:card_id])
+  # round = Round.find(params[:round_id])
+  q = (given_guess == @card.answer)
+  puts "the answer is #{q}"
+
+  guess = Guess.find_or_create_by(card: @card, round_id: 3)
+  guess.update(correct: q)
+
+  # if q
+    redirect "/decks/#{params[:deck_id]}"
+  # else
+  #   erb :question
+  # end
 
 end
 
--- db/migrate/20160415112831_create_guesses.rb
+++ db/migrate/20160415112831_create_guesses.rb
@@ -1,8 +1,11 @@
 class CreateGuesses < ActiveRecord::Migration
   def change
     create_table :guesses do |t|
-      t.boolean :first_try, default: false
+      # t.boolean :first_try, default: false # true???
       t.boolean :correct, default: false
+
+      t.references :round
+      t.references :card
 
       t.timestamps(null: false)
     end
