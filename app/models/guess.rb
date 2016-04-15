ba3cd7e8ec2b7fc4fb271c74a260e3bf28bc450c
Erica Lloyd <email": null>
2016-04-15 14:13:55 -0500

    Add model and migration for Rounds

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,4 @@
 class Guess < ActiveRecord::Base
   # Remember to create a migration!
+
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,22 @@
 class Round < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :user
+  belongs_to :deck
+  has_many   :cards, through: :decks
+  has_many   :guesses
+
+  validates :user_id, presence: true
+  validates :deck_id, presence: true
+
+  def number_right_first_try
+    cards = self.cards
+    count = cards.count { |card|
+      card.guesses.count == 1
+    }
+    count
+  end
+
+  def number_of_guesses
+    self.guesses.count
+  end
 end
