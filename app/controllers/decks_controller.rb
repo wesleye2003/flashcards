71e08313955bf25ef80917646717231a43fa7645
Erica Lloyd <email": null>
2016-04-15 17:00:54 -0500

    Add display decks controller and view

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,5 +1,12 @@
 get "/decks" do
-    @decks = Deck.all
-		erb :"decks/index"
+  @decks = Deck.all
+
+	erb :"decks/index"
+end
+
+get '/decks/:id/results' do
+ @deck = Deck.find(params[:id])
+
+ # erb:
 end
 
