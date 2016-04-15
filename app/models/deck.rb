b9d799115140db0f008152f68cde4d918ca3304b
Joshua Lugo <email": null>
2016-04-15 13:49:04 -0500

    Add initial deck model and migration

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,11 @@
 class Deck < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :rounds
+  has_many :users, through: :rounds
+  has_many :cards
+
+  def number_of_cards
+    cards = Cards.all
+    count = cards.count
+  end
 end
--- db/migrate/20160415122104_create_decks.rb
+++ db/migrate/20160415122104_create_decks.rb
@@ -1,4 +1,9 @@
 class CreateDecks < ActiveRecord::Migration
   def change
+    add_table :decks do |t|
+      t.string :name
+
+      t.timestamps, null: false
+    end
   end
 end
