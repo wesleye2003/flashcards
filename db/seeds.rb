2cfe2433d2cd940099c75b6ae389530685e3c85b
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-04-16 16:58:58 -0500

    add fun flashcard decks

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,26 +16,26 @@ erica = User.new(username: 'erica', email: 'erica@erica.com')
 erica.password = "password"
 erica.save
 
-deck_1 = Deck.create(name: 'random_deck_1')
+deck_1 = Deck.create(name: 'Hipster bands')
 
-deck_2 = Deck.create(name: 'random_deck_2')
+deck_2 = Deck.create(name: 'Developers 101')
 
-deck_3 = Deck.create(name: 'random_deck_3')
+deck_3 = Deck.create(name: 'Book genres')
 
-deck_4 = Deck.create(name: 'random_deck_4')
+deck_4 = Deck.create(name: 'Superhero powers')
 
-deck_5 = Deck.create(name: 'random_deck_5')
+deck_5 = Deck.create(name: 'Star Wars')
 
 5.times do 
 
-	deck_1.cards.create(question: Faker::Name.name, answer: "yes")
+	deck_1.cards.create(question: "Have you heard #{Faker::Hipster.words(2).join}'s latest album?", answer: "yes")
 
-	deck_2.cards.create(question: Faker::Name.name, answer: "yes")
+	deck_2.cards.create(question: "Who developed #{Faker::App.name}?", answer: Faker::App.author)
 
-	deck_3.cards.create(question: Faker::Name.name, answer: "yes")
+	deck_3.cards.create(question: "What genre does #{Faker::Book.title} belong to?", answer: Faker::Book.genre)
 
-	deck_4.cards.create(question: Faker::Name.name, answer: "yes")
+	deck_4.cards.create(question: "What power does #{Faker::Superhero.name} have?", answer: Faker::Superhero.power)
 
-	deck_5.cards.create(question: Faker::Name.name, answer: "yes")
+	deck_5.cards.create(question: "True or false: #{Faker::StarWars.character} is a #{Faker::StarWars.specie}", answer: "false")
 
 end
