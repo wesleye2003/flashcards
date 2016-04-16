041c6af9d8a6005190f631b5d72e02e08a5edd45
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-04-16 12:13:47 -0500

    Fixing critical bug

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,4 +1,4 @@
-
+get '/users/:id' do
 	#redirect home if current user tries to visit another user's profile
   redirect '/' unless params[:user_id] == current_user.id
   erb :"users/profiles/index"
